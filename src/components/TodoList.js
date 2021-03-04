f735ba85f9c2dee22aebaa866a355ae4c67eff9a
bearbearbear <bear-bear-bear@kakao.com>
2021-03-05 03:40:23 +0900

    feat: 모바일 반응형 작업
    - react-virtualized의 AutoSizer 추가

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,5 @@
 import React, { useCallback } from 'react';
-import { List } from 'react-virtualized';
+import { List, AutoSizer } from 'react-virtualized';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
@@ -21,16 +21,20 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
   );
 
   return (
-    <List
-      className="TodoList"
-      width={512} // 전체 크기
-      height={513} // 전체 높이
-      rowCount={todos.length} // 항목 개수
-      rowHeight={57} // 항목 높이
-      rowRenderer={rowRenderer} // 항목을 렌더링할 때 쓰는 함수
-      list={todos} // 배열
-      style={{ outline: 'none' }} // List에 기본 적용되는 outline 스타일 제거
-    />
+    <AutoSizer>
+      {({ height, width }) => (
+        <List
+          className="TodoList"
+          width={width}
+          height={height}
+          rowCount={todos.length} // 항목 개수
+          rowHeight={57} // 항목 높이
+          rowRenderer={rowRenderer} // 항목을 렌더링할 때 쓰는 함수
+          list={todos} // 배열
+          style={{ outline: 'none' }} // List에 기본 적용되는 outline 스타일 제거
+        />
+      )}
+    </AutoSizer>
   );
 };
 
